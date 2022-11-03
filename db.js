0e60aaafe290e8e40a581611e6bfc02f41d77578
Braun <braun@rundp.de>
2022-11-03 08:25:52 +0100

    port, for db and minor changes

--- db.js
+++ db.js
@@ -3,6 +3,7 @@ const util = require("util");
 
 var conn = mysql.createConnection({
   host: process.env.DB_URL,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PW,
   database: process.env.DB_NAME,
